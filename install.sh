81b8e0b79273d107346394e25ae469f58928b2b6
HubertK2002 <hubistef16@wp.pl>
2025-08-10 18:21:25 +0200

    Instalacja i konfiguracja nginx dla produkcji

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ bash 2.add_docker_source
 bash 3.install_packages  
 bash 4.configure_redis
 bash 5.set_permissions
+bash 6.create_nginx_site
 echo "DONE"
 echo "Checking redis"
 redis-cli -h 127.0.0.1 -p 6379 ping
