acae122a37995c00a2275eae053ce8092099c3f2
root <root@debian12.debian12>
2025-08-10 13:06:09 +0200

    Dodanie skryptu instalacyjnego

---
+++ install.sh
@@ -0,0 +1,10 @@
+cd install
+bash 1.add_docker_key
+bash 2.add_docker_source  
+bash 3.install_packages  
+bash 4.configure_redis
+echo "DONE"
+echo "Checking redis"
+redis-cli -h 127.0.0.1 -p 6379 ping
+redis-cli -h 127.0.0.1 -p 6380 ping
+
