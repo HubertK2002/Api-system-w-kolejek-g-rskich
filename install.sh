48412e732ffdda051387161ddc4742f9c31c4b13
root <root@debian12.debian12>
2025-08-10 13:16:57 +0200

    Dodanie skryptu ustawiającego uprawnienia

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ bash 1.add_docker_key
 bash 2.add_docker_source  
 bash 3.install_packages  
 bash 4.configure_redis
+bash 5.set_permissions
 echo "DONE"
 echo "Checking redis"
 redis-cli -h 127.0.0.1 -p 6379 ping
