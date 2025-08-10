937cd3cd1d4a7ae7ee6706f4ceb22b1a344fe1c2
root <root@debian12.debian12>
2025-08-10 19:17:43 +0200

    Dodanie brakujących rozszerzeń sh w pliku install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 cd install
-bash 1.add_docker_key
-bash 2.add_docker_source  
-bash 3.install_packages  
-bash 4.configure_redis
-bash 5.set_permissions
-bash 6.create_nginx_site
+bash 1.add_docker_key.sh
+bash 2.add_docker_source.sh  
+bash 3.install_packages.sh
+bash 4.configure_redis.sh
+bash 5.set_permissions.sh
+bash 6.create_nginx_site.sh
 echo "DONE"
 echo "Checking redis"
 redis-cli -h 127.0.0.1 -p 6379 ping
